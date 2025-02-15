4d4cd3cfdb7a717dfa3bb47695c14a9115fc5125
ramyasri31072006 <ramyasriachyuta@gmail.com>
2025-02-15 10:25:26 +0530

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -1,8 +1,8 @@
 #include<stdio.h>
 void bubble_sort(int a[],int n){
-	for(int i=1;i<n;i++){
-		for(int j=0;j<n-i;j++){
-			if(a[j]>a[j+1]){
+	for(int i=1;i<n;i++){// this loop runs n-1 times to sort array,after competion very loop max element of array will be sorted
+		for(int j=0;j<n-i;j++){//it helps to compare elements of unsorted array
+			if(a[j]>a[j+1]){//if present element is greater than adjacent element swap them
 				int temp=a[j];
 				a[j]=a[j+1];
 				a[j+1]=temp;
@@ -22,3 +22,5 @@ int main(){
 		printf("%d",a[i]);
 	}
 }
+//time complexity--o(n^2)
+//space complexity--O(1)
