4cd834c01656af3d2a01b0cc445849e614194ba2
ramyasri31072006 <ramyasriachyuta@gmail.com>
2025-02-15 11:21:55 +0530

    Update QuickSort.cpp

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -1,28 +1,30 @@
+/* it works on divide and conquer technique
+it chose a pivot element and divide into subarrays until the sub array reaches to single element */
 #include<stdio.h>
 void quick_sort(int a[],int lb,int ub){
 	int pivot,start,end;
 	if(lb<ub){
 	
-		pivot=lb;
+		pivot=lb;//asume  pivot element is at lower boundary
 		start=lb;
 		end=ub;
 		if(start<end){
-			while(a[start]<=a[pivot]){
+			while(a[start]<=a[pivot]){//if the element in start is <=pivot increment start
 				start++;
 			}
-			while(a[end]>a[pivot]){
+			while(a[end]>a[pivot]){//if the element in end greater than pivot decrement end
 				end--;
 			}
-			if(start<end){
+			if(start<end){//if start is < end  then swap end and start
 				int temp=a[start];
 				a[start]=a[end];
 				a[end]=temp;
 			}
 		}
-		int temp=a[pivot];
+		int temp=a[pivot];//finally swap pivot with end to get it's atual position
 		a[pivot]=a[end];
 		a[end]=temp;
-	
+	//do the same process recursivly in remaing 2 parts
 	quick_sort(a,lb,end-1);
 	quick_sort(a,end+1,ub);
 }
@@ -40,3 +42,5 @@ int main(){
 	}
 	
 }
+//time complexity--o(n^2),avg(0(nlogn))
+//space complexity--o(n)
